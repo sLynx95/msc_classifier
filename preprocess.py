f3d3f8a26aff50d2c7199fa6949090767e08aa4d
Sebastian Rys <sebastian.rys.95@gmail.com>
2018-06-06 02:21:37 +0200

    Preprocess and extraction features of songs from dir

--- preprocess.py
+++ preprocess.py
@@ -1,98 +1,125 @@
 #!/usr/bin/env python3
 
-"""Przetwarzanie wstępne utworu muzycznego"""
+"""Przetwarzanie wstępne i ekstrakcja cech utworów muzycznych"""
 
 import numpy as np
 from scipy.io import wavfile
 from scipy.fftpack import fft
-from pandas import DataFrame
+from pandas import DataFrame, read_csv, concat
+from pydub import AudioSegment
+from os import listdir
+from os.path import isfile, join
 
-# Podajemy sciezke do pliku wav
-filename = 'wav/Three_Days_Grace-The_Mountain.wav'
+# Lista na połączenie utworów z cechami
+full_dataset = DataFrame(np.array([]))
 
-# Wczytujemy plik wav
-sample_rate, stereo_sound = wavfile.read(filename)
+# Ładujemy zbiór utwór - gatunek
+dataset = read_csv('song_dataset.csv')
+dataset_noIndex = dataset.set_index('song')
+amount_songs = dataset_noIndex.size
 
-# Spłaszczamy dźwięk do mono
-mono_sound = np.mean(stereo_sound, axis=1)
+# Tworzymy listę utworów z folderu, format mp3
+files = [f for f in listdir('samplemp3') if isfile(join('samplemp3/', f))]
 
-# Pobieramy sumaryczną długość strumienia audio
-sound_length = len(mono_sound)
+# Petla po wszytkich utworach
+for file in files:
+    # Konwersja na format wav
+    sound = AudioSegment.from_mp3("samplemp3/%s" % file)
+    sound.export("wav/%s.wav" % file.split('.')[0], format="wav")
 
-# Deklarujemy szerokość okna w sekundach
-window_seconds = 4
+    # Podajemy sciezke do pliku wav
+    wave_file = ("wav/%s.wav" % file.split('.')[0])
 
-# Obliczamy szerokość okna w bitach
-window_length = window_seconds * sample_rate
+    # Wczytujemy plik wav
+    sample_rate, stereo_sound = wavfile.read(wave_file)
 
-# Wyliczamy liczbę okien
-n_windows = sound_length // window_length
+    # Spłaszczamy dźwięk do mono
+    mono_sound = np.mean(stereo_sound, axis=1)
+    
+    # Pobieramy sumaryczną długość strumienia audio
+    sound_length = len(mono_sound)
 
-# Przycinamy dźwięk do pełnych okien
-mono_sound = mono_sound[:n_windows * window_length]
-# print(np.shape(mono_sound))
+    # Deklarujemy szerokość okna w sekundach
+    window_seconds = 4
 
-# Przekształcamy audio na reprezentację z oknami w wierszach i próbkami
-# w kolumnach
-windows = mono_sound.reshape((n_windows, -1))
+    # Obliczamy szerokość okna w bitach
+    window_length = window_seconds * sample_rate
 
-# Ilość próbek utworu
-samples = np.shape(windows[-1])
-n_samples = samples[0]
+    # Wyliczamy liczbę okien
+    n_windows = sound_length // window_length
 
-# Listy na cechy okien: (szerokość pasma, czestotliwość dominujaca, moc, obwiednia)
-bandwidths, pervasives_freq, signal_strengths, signal_envelopes = [], [], [], []
-name_features = np.array(['bandwidth', 'pervasive_freq', 'signal_strength', 'signal_envelope'])
+    # Przycinamy dźwięk do pełnych okien
+    mono_sound = mono_sound[:n_windows * window_length]
+    # print(np.shape(mono_sound))
 
-# Iterujemy kolejne okna
-for i in range(n_windows):
-    # Pobieramy okno
-    window = windows[i, :]
+    # Przekształcamy audio na reprezentację z oknami w wierszach i próbkamicw kolumnach
+    windows = mono_sound.reshape((n_windows, -1))
 
-    # Wliczamy średnią z okna
-    mean_of_window = np.mean(window)
+    # Ilość próbek utworu
+    samples = np.shape(windows[-1])
+    n_samples = samples[0]
 
-    # Wyliczamy odchylenie standarowe dla okna
-    stdeviation = np.std(window)
+    # Listy na cechy okien: (szerokość pasma, czestotliwość dominujaca, moc, obwiednia)
+    bandwidths, pervasives_freq, signal_strengths, signal_envelopes = [], [], [], []
+    name_features = np.array(['bandwidth', 'pervasive_freq', 'signal_strength', 'signal_envelope'])
 
-    # Wyliczamy szybka transformate fouriera dla okna
-    fft_from_window = np.array(fft(window))
+    # Iterujemy kolejne okna
+    for i in range(n_windows):
+        # Pobieramy okno
+        window = windows[i, :]
 
-    # Potencjalne wykorzystanie filtracji medianowej/gaussowskiej
+        # Wliczamy średnią z okna
+        mean_of_window = np.mean(window)
 
-    # Z FFT wyliczmy czestotliwości dla okna
-    frequencies = np.abs(fft_from_window)
+        # Wyliczamy odchylenie standarowe dla okna
+        stdeviation = np.std(window)
 
-    # Wyliczamy szerokosc pasma dla okna
-    bandwidth = np.max(frequencies) - np.min(frequencies)
-    bandwidths.append(bandwidth)
+        # Wyliczamy szybka transformate fouriera dla okna
+        fft_from_window = np.array(fft(window))
 
-    # Wyznaczamy czestotliwość dominującą
-    if np.max(window):
-        pervasive_freq = frequencies[i]
-        pervasives_freq.append(pervasive_freq)
+        # Potencjalne wykorzystanie filtracji medianowej/gaussowskiej
 
-    # Wyznaczamy moc sygnału
-    signal_strength = np.sum(frequencies)
-    signal_strengths.append(signal_strength)
+        # Z FFT wyliczmy czestotliwości dla okna
+        frequencies = np.abs(fft_from_window)
 
-    # Wyznaczamy obwiednią sygnału dla okna (z 100 pod okien)
-    signal_envelope = np.sum([
-        (np.abs(np.mean(window[i]) - np.mean(window[i - 1]))) / (100 - 1)
-        for i in range(1, 100)
-        ])
-    signal_envelopes.append(signal_envelope)
-    """
-    # Dla pierwszego i drugiego okna rysujemy wykres przebiegu w czasie oraz częstotliwości
-    if i == 0 or i == 1:
-        fig = plt.figure(dpi=128, figsize=(10, 6))
-        plt.subplot(2, 1, 1)
-        plt.plot(window)
-        plt.subplot(2, 1, 2)
-        plt.plot(frequencies)
-        plt.show()
-    """
+        # Wyliczamy szerokosc pasma dla okna
+        bandwidth = np.max(frequencies) - np.min(frequencies)
+        bandwidths.append(bandwidth)
 
-features = DataFrame(np.transpose(np.array([
-    bandwidths, pervasives_freq, signal_strengths, signal_envelopes])), columns=name_features)
-features.to_csv('Mountain.csv')
+        # Wyznaczamy czestotliwość dominującą
+        if np.max(window):
+            pervasive_freq = frequencies[i]
+            pervasives_freq.append(pervasive_freq)
+
+        # Wyznaczamy moc sygnału
+        signal_strength = np.sum(frequencies)
+        signal_strengths.append(signal_strength)
+
+        # Wyznaczamy obwiednią sygnału dla okna (z 100 pod okien)
+        signal_envelope = np.sum([
+            (np.abs(np.mean(window[i]) - np.mean(window[i - 1]))) / (100 - 1)
+            for i in range(1, 100)
+            ])
+        signal_envelopes.append(signal_envelope)
+
+        """
+        # Dla pierwszego i drugiego okna rysujemy wykres przebiegu w czasie oraz częstotliwości
+        if i == 0 or i == 1:
+            fig = plt.figure(dpi=128, figsize=(10, 6))
+            plt.subplot(2, 1, 1)
+            plt.plot(window)
+            plt.subplot(2, 1, 2)
+            plt.plot(frequencies)
+            plt.show()
+        """
+    features = DataFrame(np.transpose(np.array([
+        bandwidths, pervasives_freq, signal_strengths, signal_envelopes])),
+        columns=name_features)
+    # features.to_csv('Mountain.csv')
+    if any(dataset['song'] == file):
+        single_song = DataFrame(dataset_noIndex.loc[[file], ['idx', 'genre']])
+        # single_song.join(features, lsuffix='_single_song', rsuffix='_features')
+        song_dataset = concat([single_song, features])
+    # full_dataset.append(song_dataset)
+
+# full_dataset.to_csv('full_dataset.csv')
