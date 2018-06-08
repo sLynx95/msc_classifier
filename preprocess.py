8dde1677f37f55677283d009c5f37be36227aaee
Sebastian Rys <sebastian.rys.95@gmail.com>
2018-06-08 18:23:31 +0200

    Fixed

--- preprocess.py
+++ preprocess.py
@@ -17,9 +17,9 @@ pd.set_option('display.width', 1000)
 # Lista na połączenie utworów z cechami
 FULL_DATASET = DataFrame(np.array([]))
 
-# Ładujemy zbiór utwór - gatunek
+# Ładujemy zbiór utwór - gatunek i ustawiamy indeks
 DATASET = read_csv('song_dataset.csv')
-DATASET_IDX = DATASET.set_index('idx')
+DATASET_IDSONG = DATASET.set_index('song')
 
 # Tworzymy listę utworów z folderu, format mp3
 FILES = [f for f in listdir('mp3') if isfile(join('mp3/', f))]
@@ -37,7 +37,7 @@ for file in FILES:
     # Wczytujemy plik wav
     sample_rate, stereo_sound = wavfile.read(wave_file)
 
-    # Spłaszczamy dźwięk do mono
+    # Spłaszczamy dźwięk do mono jeżeli byl stereo
     print('Preprocessing %s' % file)
     if sound.channels == 2:
         mono_sound = np.mean(stereo_sound, axis=1)
@@ -67,8 +67,12 @@ for file in FILES:
     n_samples = samples[0]
 
     # Listy na cechy okien: (szerokość pasma, czestotliwość dominujaca, moc, obwiednia)
-    bandwidths, pervasives_freq, signal_strengths, signal_envelopes = [], [], [], []
-    name_features = np.array(['bandwidth', 'pervasive_freq', 'signal_strength', 'signal_envelope'])
+    bandwidths, pervasives_freq, signal_strengths, signal_envelopes, genres = [], [], [], [], []
+    name_features = np.array(
+        ['genre', 'bandwidth', 'pervasive_freq', 'signal_strength', 'signal_envelope'])
+
+    # Wyciągnięcie gatunku z odpowiedniego utworu
+    genre = DATASET_IDSONG.get_value(file, 'genre')
 
     print('Extraction features of %s' % file)
     # Iterujemy kolejne okna
@@ -92,18 +96,21 @@ for file in FILES:
 
         # Wyliczamy szerokosc pasma dla okna
         bandwidth = np.max(frequencies) - np.min(frequencies)
-        if bandwidth:
+
+        # Wyznaczamy moc sygnału
+        signal_strength = np.sum(frequencies)
+
+        if bandwidth and signal_strength:
             bandwidths.append(bandwidth)
+            signal_strengths.append(signal_strength)
+            genres.append(genre)
+        else:
+            continue
 
         # Wyznaczamy czestotliwość dominującą
         if np.max(window):
             pervasive_freq = frequencies[i]
             pervasives_freq.append(pervasive_freq)
-
-        # Wyznaczamy moc sygnału
-        signal_strength = np.sum(frequencies)
-        if signal_strength:
-            signal_strengths.append(signal_strength)
 
         # Wyznaczamy obwiednią sygnału dla okna (z 100 pod okien)
         if np.max(frequencies):
@@ -123,13 +130,11 @@ for file in FILES:
             plt.show()
     """
     features = DataFrame(np.transpose(np.array([
-        bandwidths, pervasives_freq, signal_strengths, signal_envelopes])),
+        genres, bandwidths, pervasives_freq, signal_strengths, signal_envelopes])),
                          columns=name_features)
 
     if any(DATASET['song'] == file):
-        # Wyciągnięcie odpowiedniego wiersza z song_dataset i obróbka
-        single_song = DataFrame(DATASET_IDX.loc[DATASET['song'] == file])
-        single_song = single_song.reset_index(drop=True)
+        single_song = DataFrame({'song': [file]})
 
         # Połączenie utworu z cechami
         song_dataset = single_song.join(features, how='right')
